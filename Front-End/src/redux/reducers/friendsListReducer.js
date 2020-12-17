8ac49023b64069f745cec753b2e6e349bb3522e0
Oussema <oussemabendahmen@gmail.com>
2020-12-17 18:03:40 +0100

    refactor: redux reducers

--- Front-End/src/redux/reducers/friendsListReducer.js
+++ Front-End/src/redux/reducers/friendsListReducer.js
@@ -3,7 +3,8 @@ const friendsListReducer = (state = initialState, action) => {
   switch (action.type) {
     case "GET_FRIENDLIST_SUCCESS":
       return [...action.payload];
-
+    case "LOGOUT_SUCCESS":
+      return [];
     default:
       return state;
   }
